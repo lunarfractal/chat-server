1c2081e17fb9f814f169bf6a8db35118a934d151
01060c <xxs239239@gmail.com>
2025-05-28 21:28:08 +0530

    Create message.hpp

---
+++ game/message.hpp
@@ -0,0 +1,17 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+#include <string>
+#include <unordered_map>
+
+namespace game {
+
+struct Message {
+    std::u16string content;
+    std::u16string owner_nick;
+    uint16_t owner_id;
+}
+
+}
+
+#endif
