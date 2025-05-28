2c4f5f25169399be8b5ba880e696f93110aa138e
01060c <xxs239239@gmail.com>
2025-05-28 21:30:29 +0530

    Update world.hpp

--- game/world.hpp
+++ game/world.hpp
@@ -5,6 +5,8 @@
 #include <unordered_set>
 #include "../utils/utils.hpp"
 #include "player.hpp"
+#include "message.hpp"
+
 
 namespace game {
 
@@ -13,7 +15,7 @@ public:
     std::unordered_map<uint16_t, std::shared_ptr<Player>> active_players;
     std::unordered_set<uint16_t> pending_deletions;
     std::unordered_set<std::string> rooms;
-    std::unordered_map<std::string, std::unordered_set<std::string>> id2messages;
+    std::unordered_map<std::string, std::unordered_set<Message>> id2messages;
 
     void mark_for_deletion(uint16_t id) {
         pending_deletions.insert(id);
